a436be2eb5be6ec23e0a31deb2017358d8de7b00
sebastian <sebastian.mundner@gmail.com>
2017-05-06 21:20:27 +0200

    Testscript erstellt

--- services/SmartGarden/test.py
+++ services/SmartGarden/test.py
@@ -1,20 +1,24 @@
 import socket
+import struct
 from threading import Thread
 
-def tcp_rec():
-    tcp_rec_sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    tcp_rec_sock.bind(('192.168.0.187',6000));
-    tcp_rec_sock.listen(5)
-    print('waiting for data')
+ip = "192.168.0.2"
+
+def udp_rec():
+    udp_rec_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_rec_sock.bind((ip,6000));
+    print('waiting for data\n')
     while(True):
-        client, addr = tcp_rec_sock.accept()
-        print(client.recv(1024),addr)
+        data, addr = udp_rec_sock.recvfrom(1024)
+        s_recv = struct.Struct('2B 4H')
+        
+        print(s_recv.unpack(data),addr)
 
 def main():
-    t=Thread(target=tcp_rec)
+    t=Thread(target=udp_rec)
     t.start()
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind(('192.168.0.187',5998))
+    sock.bind((ip,5998))
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     
     sock.sendto('test'.encode(),('255.255.255.255',5998))
