35f9451a36ad856b0b9ea4ba042f8ee3e3cde2cc
Sebastian <sebastian.mundner@gmail.com>
2018-09-05 08:02:41 +0200

    change test script

--- services/SmartGarden/test.py
+++ services/SmartGarden/test.py
@@ -2,7 +2,7 @@ import socket
 import struct
 from threading import Thread
 
-ip = "192.168.0.2"
+ip = "192.168.4.3"
 
 def udp_rec():
     udp_rec_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -21,15 +21,20 @@ def main():
     sock.bind((ip,5998))
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     
-    sock.sendto('test'.encode(),('255.255.255.255',5998))
+    sock.sendto('\n'.encode(),('255.255.255.255',5998))
     print('start') 
     while True:
         data, addr = sock.recvfrom(1024)
-        print (data.decode(),addr)
+        print (data,addr)
+        if ip in addr:
+            continue
+        print('send data')
         tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         src_ip_addr, src_port = addr 
-        tcp_sock.connect((src_ip_addr,5999))
-        tcp_sock.send(bytes([1]))
+        tcp_sock.connect((src_ip_addr,6000))
+        rgb=bytes([255])
+        print(rgb)
+        tcp_sock.send(rgb)
         tcp_sock.close()
 
 if __name__ == '__main__':
